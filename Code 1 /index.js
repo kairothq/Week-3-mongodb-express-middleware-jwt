77382585274b5584ba914fc386d883fb83b84188
Divy Kairoth <126209124+kairothq@users.noreply.github.com>
2024-07-03 16:30:12 +0530

    Pushing on 3rd July

---
+++ Code 1 /index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const app = express();
+app.get("/health-checkup", function(req,res){
+const username = req.headers.username; // very sm that written req.query here too
+const password = req.headers.password;
+const kidneyId = req.query.kidneyId;
+
+if(username=="Divy" && password=="pass"){
+    if(kidneyId==1 || kidneyId == 2){
+
+    // do something with kidney 
+    res.json({
+        msg: "Your kidney is fine!"
+    })
+}
+}
+res.status(400).json({"msg": "Somethings up with your inputs"})
+});
+
+
+
+// const express = require("express");
+
+// const app = express();
+
+// app.get("/health-checkup", function (req, res) {
+//   const username = req.headers.username;
+//   const password = req.headers.password;
+//   const kidneyId = req.query.kidneyId;
+
+//   if (username != "harkirat" || password != "pass") {
+//     res.status(400).json({"msg": "Somethings up with your ionputs"})
+//     return
+//   }
+
+//   if (kidneyId != 1 && kidneyId != 2) {
+//     res.status(400).json({"msg": "Somethings up with your ionputs"})
+//     return
+//   }
+//   // do something with kidney here
+//   res.json({
+//     msg: "Your kidney is fine!"
+//   })
+
+  
+// });
+
+app.listen(3006);
